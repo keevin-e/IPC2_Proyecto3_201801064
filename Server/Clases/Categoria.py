f5810c8b3f4af4b9725e734f666fb880434ece36
Kevin Ruiz <2743835271202@ingenieria.usac.edu.gt>
2022-11-02 19:34:22 -0600

    modificaciones , ensayo

--- Server/Clases/Categoria.py
+++ Server/Clases/Categoria.py
@@ -14,28 +14,3 @@ class Categoria():
         self.descripcion_categoria =descripcion_categoria
         self.carga_de_trabajo = carga_de_trabajo
         self.lista_configuraciones = lista_configuraciones
-    
-    
-    def get_id_categoria (self):
-        return self.id_categoria 
-    
-    def set_id_categoria (self, id_categoria):
-        self.id_categoria = id_categoria
-    
-    def get_nombre_categoria (self):
-        return self.nombre_categoria 
-    
-    def set_nombre_categoria (self, nombre_categoria):
-        self.nombre_categoria = nombre_categoria
-    
-    def get_descripcion_categoria (self):
-        return self.descripcion_categoria 
-    
-    def set_descripcion_categoria (self, descripcion_categoria):
-        self.descripcion_categoria = descripcion_categoria
-    
-    def get_carga_de_trabajo (self):
-        return self.carga_de_trabajo 
-    
-    def set_carga_de_trabajo (self, carga_de_trabajo):
-        self.carga_de_trabajo = carga_de_trabajo
